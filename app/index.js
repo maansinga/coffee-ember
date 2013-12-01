dfe9997ed91220be9308c6261ac8702026ff54cf
Simha <simha.gemaraju@gmail.com>
2013-12-01 17:36:13 +0530

    Initial commit for generator setup

---
+++ app/index.js
@@ -0,0 +1,62 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var CoffeeEmberGenerator = module.exports = function CoffeeEmberGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(CoffeeEmberGenerator, yeoman.generators.Base);
+
+CoffeeEmberGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [{
+    type: 'confirm',
+    name: 'nameChoice',
+    message: 'Would you like to enable this option?',
+    default: true
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.nameChoice = props.nameChoice;
+
+    cb();
+  }.bind(this));
+};
+
+CoffeeEmberGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/assets');
+  this.mkdir('app/assets/stylesheets');
+  this.mkdir('app/assets/images');
+  this.mkdir('app/assets/sounds');
+  this.mkdir('app/models');
+  this.mkdir('app/controllers');
+  this.mkdir('app/components');
+  this.mkdir('app/views');
+  this.mkdir('app/routes');
+  this.mkdir('app/styles');
+  this.mkdir('app/templates');
+  this.mkdir('app/templates/partials');
+  this.mkdir('app/templates/components');
+
+  this.copy('_package.json', 'package.json');
+  this.copy('_bower.json', 'bower.json');
+};
+
+CoffeeEmberGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
